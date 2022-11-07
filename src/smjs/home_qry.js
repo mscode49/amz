d550cad15f78b3fa64a21a90aa0b4b6e756241c4
mscode49 <115342798+mscode49@users.noreply.github.com>
2022-11-07 13:22:09 +0100

    New update

--- src/smjs/home_qry.js
+++ src/smjs/home_qry.js
@@ -7,63 +7,34 @@ $().ready( function(){
 	    btn    = $('#btn_all'),
 	    btnUl  = $('#btn_all ul'),
 	    btnLi  = $('#btn_all ul li'),
-	    btnBtn = $('#btn_all ul li'),
-	    pImg   = $('.mimg');
+	    btn_La1 = $('.btn0');
 
 	if ( $(window).width() >= 361 && $(window).height() > 617 ){
+		alert("SCREEN width"+" "+ $(window).width()+ " "+ $(window).height());
+		$('body').css({});
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 		titr.css({
 			'fontSize': '40px',
-			'border': 'solid 1px yellow'
+			'border': 'solid 5px yellow'
 		});
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
-		btnBtn.css({
-			'display': 'flex',
-			'flexDirection': 'column'
-		});
-
+        btn.css({});
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+        btnLi.css({});
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+        btn_La1.css({});
 /* ################################################################################ */
 	}else if ( $(window).width() == 360 && $(window).height() == 617 ){
         btnUl.css({
         	'marginLeft': '-53px'
         });
-		alert( $(window).width() +" "+ $(window).height() + "YES............");
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 		titr.css({
 			'fontSize': '35px',
 			'border': 'solid 1px yellow'
 		});
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
-		btnBtn.css({
-			'display': 'flex',
-			'flexDirection': 'column'
-		})
 
-		pImg.on('mouseover', function(){
-			var txt = "<h2>[#] YOUR PHONE ARE NOW HACKED !!![#] Ha Ha Ha Ha Ha Ha</h2>";
-				setTimeout(function(){
-			        $('.p marquee').html(txt).css({
-			        	'marginTop': '-10%',
-			        	'color': 'red',
-			        	'textAlign': 'center',
-			        	'textShadow': '1px 2px 1px black'
-			        });
-				}, 1000);
-		
-			$('.hack', function(){
-				setTimeout(function(){
-					$('.hack').fadeOut(2000);
-					txt = "<h1> IST NUR SPAßE KEINE SORGE </h1>";
-			        $('.p marquee').html(txt).css({
-			        	'marginTop': '-10%',
-			        	'color': 'red',
-			        	'textAlign': 'center',
-			        	'textShadow': '1px 2px 1px black'
-			        });
-				}, 8000);
-			});
-			//alert("mouseover");
-		});
 		alert("Hier is "+ " "+ $(window).width()+ " "+ $(window).height());
 	}else{} /* FIN DE IF */
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
