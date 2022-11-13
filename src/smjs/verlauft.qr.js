e886bbbc2306b8e23bed6ff6e659e53b694ffeb6
mscode49 <115342798+mscode49@users.noreply.github.com>
2022-11-13 20:55:27 +0100

    Update verlauft.qr.js
    update

--- src/smjs/verlauft.qr.js
+++ src/smjs/verlauft.qr.js
@@ -1,84 +1,79 @@
+
 $().ready( function(){
-	let titr    = $('#titr-h1'),
-	    btn     = $('#btn_all'),
-	    btnUl   = $('#btn_all ul'),
-	    btnLi   = $('#btn_all ul li'),
-	    btn_La1 = $('.btn0'),
-	    tabl    = $('table');
 
-// DEFINITION DU NAVIGATEUR CHROME 
+	let btn   = $('*#btn0'),
+        btna  = $('#btn0 a'),
+        tabl0 = $('#mtabl0'),
+        tabl1 = $('*#mtabl');
 
-if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ){
-
-// DEFINITION DU NAVIGATEUR CHROME 
-}else if(navigator.userAgent.indexOf("Chrome") != -1 ){
+//##################################################################################
+if(navigator.userAgent.indexOf("Chrome") != -1 ){
+//##################################################################################
+//**********************************************************************************
+    // Windows screen Galaxy S10e
 	if ( $(window).width() == 360 && $(window).height() == 617 ){
-           btnUl.css({
-        	'marginLeft': '-53px'
-           });
-
+//**********************************************************************************
            alert("Google - Chrome");
-        }else if ( $(window).width() >= 361 && $(window).height() > 617 ){}else{}
+//**********************************************************************************
+    // Windows screen Note S20 Ultra 5G
+    }else if ( $(window).width() >= 361 && $(window).height() > 617 ){
+//**********************************************************************************
+    }else{}
+//##################################################################################
 // DEFINITION DU NAVIGATEUR SAFARIE 
 }else if(navigator.userAgent.indexOf("Safari") != -1){
-
+//##################################################################################
 // DEFINITION DU NAVIGATEUR FIREFOX 
 }else if(navigator.userAgent.indexOf("Firefox") != -1 ){
+//##################################################################################
+//**********************************************************************************
+    // Windows screen Galaxy S10e
 	if ( $(window).width() == 360 && $(window).height() == 617 ){
-           btnUl.css({
-        	'marginLeft': '-53px'
-           });
+//**********************************************************************************
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-           titr.css({
-		'fontSize': '35px',
-		'border': 'solid 1px yellow'
-           });
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-	   $('.b0').css({
-		'fontSize': '30px'
-           });
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-           btn.css({
-           	'marginLeft': '20%'
-           }) ;
+
+// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@	
+        alert("Mozila - Firefox");
+//**********************************************************************************
+    // Windows screen Note S20 Ultra 5G
+    }else if ( $(window).width() >= 361 && $(window).height() > 617 ){
+//**********************************************************************************
+        btn.css({
+            'padding': '3%',
+            'paddingLeft': '0px',
+            'paddingRight': '0px',
+            'marginBottom': '-255%'
+        });
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-           $('#btn_all .btn0 a').css({
-               'paddingRight' : '30px',
-	       'paddingLeft' : '35px'
-           });
+        btna.css({
+            'fontSize': '21px'
+        });
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-           $('#btn_all .btn1 a').css({});
+        tabl0.css({
+            'marginLeft': '17%',
+            'fontSize'  : '20px',
+            'padding': '3%'
+        });
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-           $('#btn_all .btn2 a').css({
-               'paddingRight' : '30px',
-	       'paddingLeft' : '35px'
-           });
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 	
-           alert("Mozila - Firefox");
-        }else if ( $(window).width() >= 361 && $(window).height() > 617 ){
-           btnUl.css({
-        	'marginLeft': '7.5%'
-           });
+        tabl1.css({
+            'marginLeft': '3%',
+            'fontSize'  : '18px',
+            'padding': '3%'
+        });
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-           titr.css({
-		    'fontSize': '43px',
-		    'border': 'solid 1px yellow'
-           });
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-	       $('.b0').css({
-		      'fontSize': '30px',
-		      'color': 'black',
-		      'textShadow': '1px 0.5px 1px red'
-            });
-// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
-           tabl.css({
-               'marginLeft': '0px'
-           });
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 	   alert("Mozila - Firefox");
-        }else{}
+    }else{}
+//##################################################################################
 // DEFINITION DU NAVIGATEUR INTERNET EXPLOER 
 }else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )){
+//##################################################################################
 // DEFINITION DU NAVIGATEUR INCONUE
 }else{}
+
 });
